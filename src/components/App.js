a58fd97e7b29ea681953634ec3feb7a76c54973d
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 14:57:41 -0500

    set hogList state, make GreasedFilter as child of App & pass in a callback which updates state as prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import HogList from "./HogList"
+import GreasedFilter from "./GreasedFilter";
 
 import hogs from "../porkers_data";
 
 function App() {
 
+	const [hogList, setHogList] = useState(hogs)
+
+	function handleGreasedFilter() {
+		const greasedHogs = hogList.filter(hog => hog.greased === true)
+		setHogList(greasedHogs)
+		console.log()
+	}
 
 
 	return (
 		<div className="App">
 			<Nav />
-			<HogList hogs={hogs}/>
+			<GreasedFilter onGreasedFilter={handleGreasedFilter}/>
+			<HogList hogs={hogList}/>
 		</div>
 	);
 }
