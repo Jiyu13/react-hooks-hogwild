4d86de221b8062beb656e8bd382b77868a87f36a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 14:34:50 -0500

    toggle hog details, add showDetails state & onClick event

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -1,10 +1,20 @@
+import { useState } from "react"
 
 function Hog({ name, specialty, greased, weight, image}) {
+
+    const [showDetails, setShowDetails] = useState(false)
+    const isDisplay = showDetails ? "" : "none"
+
+    function handleToggleDetails() {
+        setShowDetails(show => !show)
+        console.log(showDetails)
+    }
+
     return (
         <div className="ui eight wide column pigTile">
             <div id={name}>{name}</div>
-            <img src={image} alt={name} />
-            <div className="details">
+            <img src={image} alt={name} onClick={handleToggleDetails}/>
+            <div className="details" style={{display:isDisplay}}>
                 <p name={specialty}>{specialty}</p>
                 <p name={weight}>{weight}</p>
                 <p name="greased">{greased}</p>
