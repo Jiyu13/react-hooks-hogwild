0373da82e1e03174e3ffcc1d6fe8b5c26ccb7f22
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 16:26:55 -0500

    add name attritube to each input

--- src/components/NewHogForm.js
+++ src/components/NewHogForm.js
@@ -11,27 +11,25 @@ function NewHogForm() {
         image: "",
     })
 
-
-
     return (
         <form className="new-hog-form">
             <label>Name</label>
-            <input type="text" value={formData.name}/>
+            <input type="text" value={formData.name} name="name"/>
 
             <label>Specialty</label>
-            <input type="text" value={formData.specialty}/>
+            <input type="text" value={formData.specialty} name="specialty"/>
 
             <label>Greased</label>
-            <input type="checkbox" value={greased}/>
+            <input type="checkbox" value={greased} name="greased"/>
 
             <label>Weight</label>
-            <input type="text" value={weight}/>
+            <input type="text" value={weight} name="weight"/>
 
             <label>Highest Medal Achieved</label>
-            <input type="text" value={"highest medal achieved"}/>
+            <input type="text" value={formData["highest medal achieved"]} name="highest medal achieved"/>
 
             <label>Image</label>
-            <input type="text" value={formData.image}/>
+            <input type="text" value={formData.image} name="image"/>
 
             <input type="submit" value="Add Hog!" />
 
