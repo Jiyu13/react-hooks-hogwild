85ab1ae08c0dd058133fa224eedadc9533dfc018
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 15:52:48 -0500

    make Sort as child of App, set new state-sortBy, create helper function for Sort, sort hogList by name/weight, update Hoglist props

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,44 @@ import React, { useState } from "react";
 import Nav from "./Nav";
 import HogList from "./HogList"
 import GreasedFilter from "./GreasedFilter";
+import Sort from "./Sort";
 
 import hogs from "../porkers_data";
 
 function App() {
 
 	const [hogList, setHogList] = useState(hogs)
+	const [sortBy, setSortBy] = useState("All")
 
 	function handleGreasedFilter() {
 		const greasedHogs = hogList.filter(hog => hog.greased === true)
 		setHogList(greasedHogs)
-		console.log()
 	}
+
+	function handleSort(event) {
+		setSortBy(event.target.value)
+	}
+
+	console.log(sortBy)
+	const sortArray = hogList.sort((a, b) => {
+		if (sortBy === "name") {
+			return a.name.localeCompare(b.name)
+		} else if (sortBy === "weight") {
+			console.log(a.weight, b.weight)
+			return a.weight.toString().localeCompare(b.weight.toString())
+		} else {
+			return hogList
+		}
+	})
+	console.log(sortArray)
 
 
 	return (
 		<div className="App">
 			<Nav />
+			<Sort onSortChange={handleSort} sort={sortBy}/>
 			<GreasedFilter onGreasedFilter={handleGreasedFilter}/>
-			<HogList hogs={hogList}/>
+			<HogList hogs={sortArray}/>
 		</div>
 	);
 }
