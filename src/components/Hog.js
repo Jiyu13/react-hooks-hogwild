431d47f47263bfc5e5e905fe25b04f91e96f0303
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 17:54:46 -0500

    add <button> with onClick handler to hide hog, set new state showHog, update showHog when button is cliked and hide hog

--- src/components/Hog.js
+++ src/components/Hog.js
@@ -2,25 +2,42 @@ import { useState } from "react"
 
 function Hog({ name, specialty, greased, weight, image, medal}) {
 
+    const [hideHog, setIsHide] = useState(false)
     const [showDetails, setShowDetails] = useState(false)
     const isDisplay = showDetails ? "" : "none"
     const isGreased = greased ? "Greased" : "Not greased"
+    const isHide = hideHog ? "none" : ""
+
 
     function handleToggleDetails() {
         setShowDetails(show => !show)
     }
 
+    function handleHide() {
+        setIsHide(hide => !hide)
+        console.log(hideHog)
+    }
+
     return (
-        <div className="ui eight wide column pigTile" name={name} id={name}>
-            <div>{name}</div>
-            <img src={image} alt={name} onClick={handleToggleDetails}/>
-            <div className="details" style={{display:isDisplay}}>
-                <p name={specialty}>{specialty}</p>
-                <p name={weight}>{weight}</p>
-                <p name="greased">{isGreased}</p>
-                <p>Highest Medal Achieved: {medal}</p>
-            </div>
-        </div>
+        <>
+
+                <div className="ui eight wide column pigTile" name={name} id={name} style={{display:isHide}}>
+                    <button onClick={handleHide}>Hide This Hog</button>
+                    <div>{name}</div>
+                    <img src={image} alt={name} onClick={handleToggleDetails}/>
+                    <div className="details" style={{display:isDisplay}}>
+                        <p name={specialty}>{specialty}</p>
+                        <p name={weight}>{weight}</p>
+                        <p name="greased">{isGreased}</p>
+                        <p>Highest Medal Achieved: {medal}</p>
+                    </div>
+                </div>
+
+        </>
+
+        
+
+
     )
 }
 
