ae2a6f44813c0a2f8395a6de497868ac3f4df899
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 16:29:05 -0500

    add onChange to each input and pass in handleInput which will update formData with new data

--- src/components/NewHogForm.js
+++ src/components/NewHogForm.js
@@ -11,25 +11,31 @@ function NewHogForm() {
         image: "",
     })
 
+    function handleInput(event) {
+        const name = event.target.name
+        const value = event.target.value
+        setFormData(...formData, [name]: value)
+    }
+
     return (
         <form className="new-hog-form">
             <label>Name</label>
-            <input type="text" value={formData.name} name="name"/>
+            <input type="text" value={formData.name} name="name" onChange={handleInput}/>
 
             <label>Specialty</label>
-            <input type="text" value={formData.specialty} name="specialty"/>
+            <input type="text" value={formData.specialty} name="specialty" onChange={handleInput}/>
 
             <label>Greased</label>
-            <input type="checkbox" value={greased} name="greased"/>
+            <input type="checkbox" value={greased} name="greased" onChange={handleInput}/>
 
             <label>Weight</label>
-            <input type="text" value={weight} name="weight"/>
+            <input type="text" value={weight} name="weight" onChange={handleInput}/>
 
             <label>Highest Medal Achieved</label>
-            <input type="text" value={formData["highest medal achieved"]} name="highest medal achieved"/>
+            <input type="text" value={formData["highest medal achieved"]} name="highest medal achieved" onChange={handleInput}/>
 
             <label>Image</label>
-            <input type="text" value={formData.image} name="image"/>
+            <input type="text" value={formData.image} name="image" onChange={handleInput}/>
 
             <input type="submit" value="Add Hog!" />
 
